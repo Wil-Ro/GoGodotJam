839df55aa85cc7930e8d32b98cb15c35e73780e2
Nye Evans <nyeevans@outlook.com>
2022-05-29 00:07:50 +0100

    -finalises random level gen
    - adds final code to first draft of level gen
    - adds enemy template
    - adds some test rooms

---
+++ Enemies/GenericEnemy.gd
@@ -0,0 +1,20 @@
+extends KinematicBody
+
+# ---changing values---
+# base
+export var base_speed = 0 # normal walking speed
+export var damage = 0 # damage dealt
+export var health = 0 # health
+export var sight_chance = 0 # how likely it is to notice the player
+export var sight_radius = 0 # length of sight
+
+# extra abilities
+export var dash_speed = 0 # speed when doing sudden dash to player, 0 to not dash
+
+
+# ---code---
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	pass
--- Level/LevelGen.gd
+++ Level/LevelGen.gd
@@ -6,21 +6,40 @@ export(PackedScene) var Player
 
 
 func _ready():
+	# set-up
 	randomize()
+	
+	# calculate enemies
+	
+	
+	# build level
 	generateArena()
+	
+	# add player
 	var player = Player.instance()
 	add_child(player)
-	player.translation = Vector3(0, 3, 0)
+	player.translation = Vector3(0, 1, 0)
 
 func generateArena():
-	var currentAddLocation = Vector3.ZERO
-	for i in range((randi() % 4) + 1):
-		print ("making 1")
-		# create and store a random section
-		var currentSection = sections[randi() % sections.size()].instance()
+	var currentAddLocation = Transform()
+	var valCache = [-1, -1] # we put -1 in here so that randomVal is in the cache to start. We use -1 because it wont mess with anything
+	var randomVal = -1
+	for i in range(5): #(randi() % 4) + 1
+		# generate a nice random number
+		while randomVal in valCache:
+			randomVal = randi() % sections.size()
+		valCache.push_front(randomVal)
+		valCache.pop_back()
+		
+		#create and store section
+		var currentSection = sections[randomVal].instance()
 		add_child(currentSection)
 		
-		# we have a position called connector and one called reciever, we want the con and rec to connect the sections
-		currentSection.translation = currentAddLocation
-		currentAddLocation = currentSection.get_node("Connector").translation
+		# move section
+		currentSection.transform = currentAddLocation
+		if i != 0:
+			currentSection.transform = currentSection.transform.translated(Vector3.ZERO - currentSection.get_node("Reciever").translation)
+		currentAddLocation = currentSection.get_node("Connector").global_transform
+		
+		# add enemies to section
 
