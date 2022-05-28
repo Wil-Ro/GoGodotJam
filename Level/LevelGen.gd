ef6efc83312292b2053d248432be366c44d7f551
Nye Evans <nyeevans@outlook.com>
2022-05-28 02:43:56 +0100

    Adds prototype for random level gen
    -Adds script that places empty room sections

--- Level/LevelGen.gd
+++ Level/LevelGen.gd
@@ -1,14 +1,26 @@
+# this should generate the levels and enemies
 extends Spatial
 
-export(PackedScene) var sections
+export(Array, PackedScene) var sections
+export(PackedScene) var Player
+
 
 func _ready():
+	randomize()
 	generateArena()
-	
-func _process(delta):
-	pass
+	var player = Player.instance()
+	add_child(player)
+	player.translation = Vector3(0, 3, 0)
 
 func generateArena():
-	pass
+	var currentAddLocation = Vector3.ZERO
+	for i in range((randi() % 4) + 1):
+		print ("making 1")
+		# create and store a random section
+		var currentSection = sections[randi() % sections.size()].instance()
+		add_child(currentSection)
+		
+		# we have a position called connector and one called reciever, we want the con and rec to connect the sections
+		currentSection.translation = currentAddLocation
+		currentAddLocation = currentSection.get_node("Connector").translation
 
-# this should generate the levels and enemies
--- SceneContainer.gd
+++ SceneContainer.gd
@@ -1,3 +1,1 @@
 extends Spatial
-
-export(PackedScene) var sections
